9b997b5c2a2b7f0612916bcd42ae7c683a2810dc
William Sercombe <wsercombe@gmail.com>
2021-09-25 18:42:42 -0700

    Style new order page

--- VendorOrder/Controllers/VendorsController.cs
+++ VendorOrder/Controllers/VendorsController.cs
@@ -40,11 +40,11 @@ namespace VendorOrder.Controllers
     }
 
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderDescription)
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, string orderPrice, string orderDate)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderDescription);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
